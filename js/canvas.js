b14a68938b05f31967ce4ba6cfcea32a85f59ae5
Leonardo FG <leonardo.fg@engineer.com>
2022-02-24 23:43:53 -0300

    Update canvas.js

---
+++ js/canvas.js
@@ -0,0 +1,10 @@
+
+var tela = document.querySelector('canvas');
+console.log(tela);
+var pincel = tela.getContext('2d');
+
+function desenhaTela() {
+        //retângulo verde
+        pincel.fillStyle = 'grey';
+        pincel.fillRect(0, 0, 800, 1200);
+}
