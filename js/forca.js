e13ab58c3b5d9934dc27b081d5c101658c1f1500
Leonardo FG <leonardo.fg@engineer.com>
2022-03-04 02:05:38 -0300

    Update forca.js

--- js/forca.js
+++ js/forca.js
@@ -33,80 +33,71 @@ function iniciaJogo(event) {
         console.log('posição tecla digitada chuteCerto: ',teclaP.indexOf(chuteCerto));
         console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
         */
-    if (vidas != 0){
-        let teste = sorteio.indexOf(teclaP);
-        console.log('acertou a letra:', teclaP);
-        console.log('primeira posição letra acertada: ', teste);
-        if (sorteio.indexOf(teclaP) >= 0){
-            console.log('posição tecla digitada chuteCerto: ',chuteCerto.indexOf(teclaP));
-            if (chuteCerto.indexOf(teclaP) >= 0){
-                console.log('Você já ACERTOU essa Letra!');
-            } else {
-                chuteCerto.push(teclaP);
-                console.log('Letras Acertadas: ', chuteCerto);
-                for (let i = 0; i < sorteio.length; i++){
-                    var posi = sorteio.indexOf(teclaP, i);
-                    if (posi >= 0 ){
-                        i = posi;
-                        desenhaLetraPos(letraDes, posi);
-                        pos.push(posi);
-                        posT.push(posi);
+        if (vidas != 0){
+            let teste = sorteio.indexOf(teclaP);
+            console.log('acertou a letra:', teclaP);
+            console.log('primeira posição letra acertada: ', teste);
+            if (sorteio.indexOf(teclaP) >= 0){
+                console.log('posição tecla digitada chuteCerto: ',chuteCerto.indexOf(teclaP));
+                if (chuteCerto.indexOf(teclaP) >= 0){
+                    console.log('Você já ACERTOU essa Letra!');
+                } else {
+                    chuteCerto.push(teclaP);
+                    console.log('Letras Acertadas: ', chuteCerto);
+                    for (let i = 0; i < sorteio.length; i++){
+                        var posi = sorteio.indexOf(teclaP, i);
+                        if (posi >= 0 ){
+                            i = posi;
+                            desenhaLetraPos(letraDes, posi);
+                            pos.push(posi);
+                            posT.push(posi);
+                        }
+                        console.log(sorteio[i]);
+                        console.log(posi);
+                        console.log(pos);
+                        console.log(posT);
                     }
-                    console.log(sorteio[i]);
-                    console.log(posi);
-                    console.log(pos);
-                    console.log(posT);
+
+                    pos = [];
+                    if (posT.length == sorteio.length){
+                        alert('Você Ganhou !!!');
+                    }
                 }
-                
-                pos = [];
-                if (posT.length == sorteio.length){
-                    alert('Você Ganhou !!!')
-                }
-            }
-        } else {
-            if (chuteErro.indexOf(teclaP) >= 0){
-                console.log('Você já ERROU essa Letra!')
             } else {
-                console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
-                vidas = vidas - 1;
-                console.log('vidas:', vidas);
-                chuteErro.push(teclaP);
-                console.log('Letras erradas: ', chuteErro);
+                if (chuteErro.indexOf(teclaP) >= 0){
+                    console.log('Você já ERROU essa Letra!')
+                } else {
+                    console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
+                    vidas = vidas - 1;
+                    console.log('vidas:', vidas);
+                    chuteErro.push(teclaP);
+                    console.log('Letras erradas: ', chuteErro);
+                    posiE = chuteErro.length;
+                    desenhaLetraErro(letraDes, posiE);
+                }
             }
         }
-    } 
-    if (vidas == 5){
-        cabeca();
-    }
-    if (vidas == 4){
-        corpo();
-    }
-    if (vidas == 3){
-        bracoEsquerdo();
-    }
-    if (vidas == 2){
-        bracoDireito();
-    }
-    if (vidas == 1){
-        pernaEsquerda();
-    }
-    if (vidas == 0){
-        pernaDireita();
-        alert('Você morreu !');
-    }
-       
-        /*se sorteio[posição do for] for igual a teclaP (palavra que estou procurando)
-        então coloco na variavel palavra[posicao do for] o conteudo de teclaP
-        quando a variavel -> palavra for igual a variavel -> sorteio, a palavra foi descoberta
-        */
-
-
+        if (vidas == 5){
+            cabeca();
+        }
+        if (vidas == 4){
+            corpo();
+        }
+        if (vidas == 3){
+            bracoEsquerdo();
+        }
+        if (vidas == 2){
+            bracoDireito();
+        }
+        if (vidas == 1){
+            pernaEsquerda();
+        }
+        if (vidas == 0){
+            pernaDireita();
+            alert('Você morreu !');
+        }
     });
-
-
-
 }
-
 
 // botão adiciona palavra
 let addPalavra= document.querySelector('#nova-palavra');
@@ -125,12 +116,3 @@ function adicionaPalavra(event) {
     }
 }
 
-
-// campo entrada palavra
-//let novaPalavra = document.querySelector('#input-nova-palavra').value;
-/*novaPalavra.addEventListener('click', novaPalavraa)
-
-function novaPalavraa() {
-    event.preventDefault(); //para não carregar a pagina de no após clicar e limpar os campos
-    console.log('campo Adiciona Palavra foi clicado')
-}*/
