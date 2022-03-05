6e5b4ce356c956d9f555a0d7b9dad5cc202354d4
Leonardo FG <leonardo.fg@engineer.com>
2022-03-05 02:54:46 -0300

    Update forca.js

--- js/forca.js
+++ js/forca.js
@@ -7,7 +7,7 @@ let sorteio;
 let pos =[];
 let posT =[];
 let letraDes;
-let ganhou;
+let estamosEmJogo = true;
 
 
 // botão inicia jogo
@@ -16,16 +16,21 @@ botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo(event) {
     event.preventDefault(); //para não carregar a pagina de no após clicar e limpar os campos
-    console.log('Botão começar jogo foi clicado');
+    //console.log('Botão começar jogo foi clicado');
     sorteio = palavras[Math.floor(Math.random()*palavras.length)];
-    console.log('Palavra sorteada: ', sorteio);
-    console.log('Número de caracteres na palavra: ', sorteio.length);
+    //console.log('Palavra sorteada: ', sorteio);
+    //console.log('Número de caracteres na palavra: ', sorteio.length);
     nTracos = sorteio.length;
+    estamosEmJogo = true;
     forca();
     desenhaTracos(nTracos);
 
     document.addEventListener("keypress", function(event) {
-        let teclaP = event.key;
+        let teclaCheck = event.key;
+        let letraValida = /^[a-zA-Z]+$/.test(teclaCheck);
+        
+        let teclaP = letraValida ? teclaCheck : '';
+                
         teclaP = teclaP.toUpperCase();
         letraDes = teclaP;
         /*
@@ -34,18 +39,19 @@ function iniciaJogo(event) {
         console.log('posição tecla digitada chuteCerto: ',teclaP.indexOf(chuteCerto));
         console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
         */
-        if (vidas != 0){
+
+        if (vidas != 0 && estamosEmJogo == true && teclaP.length > 0){
             let teste = sorteio.indexOf(teclaP);
-            console.log('acertou a letra:', teclaP);
-            console.log('primeira posição letra acertada: ', teste);
+            /*console.log('acertou a letra:', teclaP);
+            console.log('primeira posição letra acertada: ', teste);*/
             if (sorteio.indexOf(teclaP) >= 0){
-                console.log('posição tecla digitada chuteCerto: ',chuteCerto.indexOf(teclaP));
+                //console.log('posição tecla digitada chuteCerto: ',chuteCerto.indexOf(teclaP));
                 if (chuteCerto.indexOf(teclaP) >= 0){
                     console.log('Você já ACERTOU essa Letra!');
                     //alert('Você já ACERTOU essa Letra!');
                 } else {
                     chuteCerto.push(teclaP);
-                    console.log('Letras Acertadas: ', chuteCerto);
+                    //console.log('Letras Acertadas: ', chuteCerto);
                     for (let i = 0; i < sorteio.length; i++){
                         var posi = sorteio.indexOf(teclaP, i);
                         if (posi >= 0 ){
@@ -54,16 +60,17 @@ function iniciaJogo(event) {
                             pos.push(posi);
                             posT.push(posi);
                         }
-                        console.log(sorteio[i]);
+                        /*console.log(sorteio[i]);
                         console.log(posi);
                         console.log(pos);
-                        console.log(posT);
+                        console.log(posT);*/
                     }
 
                     pos = [];
                     if (posT.length >= sorteio.length){
                         console.log('Você Ganhou !!!')
                         //alert('Você Ganhou !!!');
+                        estamosEmJogo = false;
                     }
                 }
             } else {
@@ -71,53 +78,64 @@ function iniciaJogo(event) {
                     console.log('Você já ERROU essa Letra!');
                     //alert('Você já ERROU essa Letra!');
                 } else {
-                    console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
+                    //console.log('posição tecla digitada chuteErro: ',teclaP.indexOf(chuteErro));
                     vidas = vidas - 1;
-                    console.log('vidas:', vidas);
+                    //console.log('vidas:', vidas);
                     chuteErro.push(teclaP);
-                    console.log('Letras erradas: ', chuteErro);
+                    //console.log('Letras erradas: ', chuteErro);
                     posiE = chuteErro.length;
                     desenhaLetraErro(letraDes, posiE);
                 }
             }
+        } else {
+            console.log("estou aqui no else !!")
         }
-        if (vidas == 5){
-            cabeca();
+        switch (vidas) {
+            case 5:
+                cabeca();
+                break;
+            case 4:
+                corpo();
+                break;
+            case 3:
+                bracoDireito();
+                break;
+            case 2:
+                bracoEsquerdo();
+                break;
+            case 1:
+                pernaDireita();
+                break;
+            case 0:
+                pernaEsquerda();
+                estamosEmJogo = false;
+                console.log('Você morreu !');
+                break;
         }
-        if (vidas == 4){
-            corpo();
-        }
-        if (vidas == 3){
-            bracoEsquerdo();
-        }
-        if (vidas == 2){
-            bracoDireito();
-        }
-        if (vidas == 1){
-            pernaEsquerda();
-        }
-        if (vidas == 0){
-            pernaDireita();
-            //alert('Você morreu !');
-            console.log('Você morreu !');
-        }
-    });
+
+        });
 }
 
 // botão adiciona palavra
-let addPalavra= document.querySelector('#nova-palavra');
+let addPalavra= document.querySelector('#nova-palavra-btn');
 addPalavra.addEventListener('click', adicionaPalavra);
 
 function adicionaPalavra(event) {
+
     event.preventDefault(); //para não carregar a pagina de no após clicar e limpar os campos
     let novaPalavra = document.querySelector('#input-nova-palavra').value;
     console.log('novapalavra:', novaPalavra);
-    if (novaPalavra.length <= 8) {
+
+    let palavraValida = /^[a-zA-Z]+$/.test(novaPalavra);
+    //onsole.log(palavraValida);
+    //let result = palavraValida ? teclaCheck : '';
+
+    if (novaPalavra.length <= 8 && palavraValida == true) {
         let result = novaPalavra.toUpperCase(); //converte para maiusculo
         palavras.push(result);
         console.log(palavras);
     } else {
-        alert('a palavra deve ter no máximo 8 caracteres');
+        alert('a palavra deve ter no máximo 8 caracteres e não pode conter caracteres especiais ou numeros');
     }
 }
 
